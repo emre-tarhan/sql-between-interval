a04d1af5240036da367671a85d2c8dd8e3cbfe25
Emre Tarhan <106887102+emre-tarhan@users.noreply.github.com>
2022-09-03 08:32:38 +0300

    Update between-interval.php

--- between-interval.php
+++ between-interval.php
@@ -6,6 +6,7 @@ $data = $db->query("SELECT * FROM table WHERE eklenme_tarihi BETWEEN NOW() - INT
 $data = $db->query("SELECT * FROM table WHERE eklenme_tarihi BETWEEN NOW() - INTERVAL 30 DAY AND NOW() ORDER BY eklenme_tarihi ASC");
 
 
-# Bu filtrelenen verilerin sayısı (Son x günde kaç tane ürün girildi? Sorusunun cevabı);
+# Bu filtrelenen verilerin sayısı (Son x günde kaç tane ürün girildi? Sorusunun cevabı)
+# rowCount() ifadesi bize satır sayısını söyler.
 $data->rowCount();
 
