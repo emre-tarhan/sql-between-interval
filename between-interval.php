3998ed6916a2dfce64ae29e0ab4dd20aaca5ee6c
Emre Tarhan <106887102+emre-tarhan@users.noreply.github.com>
2022-09-03 08:31:53 +0300

    Update between-interval.php

--- between-interval.php
+++ between-interval.php
@@ -1,11 +1,11 @@
 <?php
-# Son bir haftalık veri
+# Son bir haftada girilen veriyi filtreleme
 $data = $db->query("SELECT * FROM table WHERE eklenme_tarihi BETWEEN NOW() - INTERVAL 7 DAY AND NOW() ORDER BY eklenme_tarihi ASC");
 
-# Son bir aylık veri
+# Son bir ayda girilen veriyi filtreleme
 $data = $db->query("SELECT * FROM table WHERE eklenme_tarihi BETWEEN NOW() - INTERVAL 30 DAY AND NOW() ORDER BY eklenme_tarihi ASC");
 
 
-# Son x günde tabloya kaç adet veri girildiğini göstermek için;
+# Bu filtrelenen verilerin sayısı (Son x günde kaç tane ürün girildi? Sorusunun cevabı);
 $data->rowCount();
 
